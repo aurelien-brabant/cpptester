9084ea4550be81096fdf3aa20c86ae870eb7de99
abrabant <abrabant@student.42.fr>
2021-10-04 11:41:47 +0200

    Change test suite name printing color

--- src/Tester.cpp
+++ src/Tester.cpp
@@ -79,7 +79,7 @@ bool Tester::runAllSuites(void)
 	}
 
 	for (TestSuiteMap::const_iterator cit = _testSuites.begin(); cit != _testSuites.end(); ++cit) {
-		cout << "\033[1;30m>>>>>>>>>> \033[0;36m" << cit->first << " \033[1;30m<<<<<<<<<<\033[0m\n\n";
+		cout << "\033[1;0m>>>>>>>>>> \033[0;36m" << cit->first << " \033[1;0m<<<<<<<<<<\033[0m\n\n";
 
 		/* For each test */
 		size_t passedN = 0;
